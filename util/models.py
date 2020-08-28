1b7216d99df52b045c859e42bd12e874a893019f
wzmJimmy <zwang2236@wisc.edu>
2020-08-28 20:51:50 +0800

    try subclass model

--- util/models.py
+++ util/models.py
@@ -3,6 +3,8 @@ from .layers_loss_metrics import GeMPoolingLayer,ArcFace,ArcFaceLoss_Metrics
 
 import tensorflow as tf
 import efficientnet.tfkeras as efn
+from tensorflow.python.keras.engine import compile_utils
+from tensorflow.python.util import nest
 
 
 models = {"DenseNet201":tf.keras.applications.DenseNet201,
@@ -61,3 +63,160 @@ class Efn_Gem_Arc_builder:
             ArcFace(nclasses2,dtype=tf.float32,name = "ArcFace"+suffix)
         ])
         return model
+
+class Branches_builder:
+    @staticmethod
+    def build_outclass_detector(embed,nclass=2):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Input(shape=(embed,)),
+            tf.keras.layers.Dense(nclass, activation="softmax", 
+                        dtype=tf.float32,name = "inclass"),
+        ])
+        return model
+
+
+class Model_w_self_backpropagated_branches(keras.Model):
+    dic_type_2num = {
+        "normal": 1,
+        "normal_weight": 2,
+        "auto_encoder": 0,
+        "auto_encoder_weight": 1
+    }
+
+    def __init__(self, stem, branches, input_layer_names,
+                train_type, valid_type):
+        super(Model_w_self_backpropagated_branches, self).__init__()
+        self.stem = stem
+        self.branches = branches
+        self.input_layer_names = input_layer_names
+
+        assert len(branches)==len(input_layer_names)
+        assert len(branches)==len(train_type)
+        assert len(branches)==len(valid_type)
+        self.num_models = len(self.branches) + 1
+
+        if (any(i not in dic_type_2num for i in train_type) or 
+            any(i not in dic_type_2num for i in valid_type)):
+            raise ValueError("train_type and valid_type must be one of {}".
+                            format(self.get_type()))
+
+        self.train_type = train_type
+        self.valid_type = valid_type
+
+    @staticmethod
+    def get_type():
+        return list(dic_type_2num.keys())
+
+    def _get_branches_input(self,idx):
+        return self.stem.get_layer(name=self.input_layer_names[idx]).output
+
+    def sep_input(self,data,types):
+        assert self.num_models == 1 + sum(self.dic_type_2num(i) for i in types)
+        inputs = [None]*self.num_models
+        main_input = data[0]
+
+        inputs,label_idx,weight_id = [],0,sum(key.startswith("normal") for key in types)
+        for i,key in enumerate(types):
+            dic = {}
+            if key == "normal":
+                label_idx += 1
+                dic["label"] = data[label_idx]
+            elif key == "normal_weight":
+                label_idx += 1
+                weight_id += 1
+                dic["label"] = data[label_idx]
+                dic["weight"] = data[weight_id]
+            elif key == "auto_encoder_weight":
+                weight_id += 1
+                dic["weight"] = data[weight_id]
+
+            if i==1: dic["input"] = main_input
+            inputs.append(dic)
+
+        return inputs
+
+    def compile(self, optimizers, loss_fns, metrics=None):
+        super(Model_w_self_backpropagated_branches, self).compile()
+        assert len(optimizers)==len(loss_fns)
+        assert len(optimizers)==self.num_models
+
+        self.loss_fns = loss_fns
+        self.metrics = metrics
+        # TODO: support multiple metrics for one branch.
+ 
+        with self.distribute_strategy.scope():
+            self._validate_compile(optimizers, self.metrics)
+
+            self.optimizers = self._get_optimizer(optimizers)
+            self.compiled_loss = [compile_utils.LossesContainer(
+                loss, output_names=self.output_names) 
+                for loss in self.loss_fns]
+            self.compiled_metrics = [compile_utils.MetricsContainer(
+                metric, output_names=self.output_names)
+                for metric in self.metrics]
+
+    @property
+    def metrics(self):
+        metrics = []
+        if self.compiled_loss is not None:
+            metrics += nest.flatten(self.compiled_loss.metrics)
+        if self.compiled_metrics is not None:
+            metrics += nest.flatten(self.compiled_metrics.metrics)
+
+        for l in self._flatten_layers():
+            metrics.extend(l._metrics)  # pylint: disable=protected-access
+        return metrics
+
+
+    def train_step(self, data):
+        inputs = self.sep_input(data,self.train_type)
+        
+        for i in range(self.num_models):
+            ds_input = inputs[i]
+            ts_input = ds_input.get("input", self._get_branches_input(i)) 
+            label = ds_input.get("label", ts_input)
+            sample_weight = ds_input.get("weight",None)
+            model = self.stem if i==0 else self.branches[i-1]
+            
+            with tf.GradientTape() as tape:
+                predictions = model(ts_input)
+                loss = self.compiled_loss[i](
+                    label, predictions,
+                    sample_weight=sample_weight,
+                )
+            variable = model.trainable_weights
+            grads = tape.gradient(loss, variable)
+            self.optimizers[i].apply_gradients(zip(grads, variable))
+            self.compiled_metrics[i].update_state(label, predictions, sample_weight=sample_weight)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        ts_input = data[0]
+        y_preds = self(ts_input, training=False)
+
+        inputs = self.sep_input(data,self.valid_type)
+        for i in range(self.num_models):
+            ds_input = inputs[i]
+            label = ds_input.get("label", self._get_branches_input(i))
+            sample_weight = ds_input.get("weight",None)
+            self.compiled_loss[i](label, y_preds[i],
+                    sample_weight=sample_weight)
+            self.compiled_metrics[i].update_state(label, y_preds[i],
+                    sample_weight=sample_weight)
+        
+        return {m.name: m.result() for m in self.metrics}
+
+    def call(self, inputs, training=None, mask=None):
+        res = []
+
+        for i in range(self.num_models):
+            if i==0:
+                ts_input = inputs
+                model = self.stem
+            else:
+                ts_input = self._get_branches_input(i)
+                model = self.branches[i-1]
+            res.append(model(ts_input,training=training))
+
+        return res
