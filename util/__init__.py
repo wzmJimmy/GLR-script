f1a7d102765c807217b6c675531daba7862727e7
wzmJimmy <zwang2236@wisc.edu>
2020-08-30 09:16:32 +0800

    init fix version

--- util/__init__.py
+++ util/__init__.py
@@ -2,4 +2,8 @@ from .gcs_client import GCSclient
 from .opt_callbacks import optimizer_h5,NBatchProgBarLogger,NEpochModelCheckpoint_wOptimizer
 from .layers_loss_metrics import GeMPoolingLayer,ArcFace,ArcFaceLoss_Metrics
 from .dataset_parser import Lookup,Preprocess
-from .models import Efn_Gem_Arc_builder,Branches_builder,Model_w_self_backpropagated_branches,Transfer_builder
+from .models import Efn_Gem_Arc_builder,Branches_builder,Transfer_builder
+
+import tensorflow as tf
+if tf.__version__>="2.2.0":
+    from .models import Model_w_self_backpropagated_branches
